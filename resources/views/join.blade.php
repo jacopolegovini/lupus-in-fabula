5e419e74d4c74d0196dd22d2ce73be6b6adbc6e1
jacopolegovini <legovini96@gmail.com>
2025-12-28 17:07:36 +0100

    migration and model player created

---
+++ resources/views/join.blade.php
@@ -0,0 +1,6 @@
+<form method="POST" action="/join">
+    @csrf
+    <input name="name" placeholder="Nome">
+    <input name="code" placeholder="Codice stanza">
+    <button>Entra</button>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,28 @@ Route::post('/create-room', function () {
 
     return "Stanza creata! Codice: " . $room->code;
 });
+
+use App\Models\Player;
+
+Route::get('/join', function () {
+    return view('join');
+});
+
+Route::post('/join', function (Illuminate\Http\Request $request) {
+    $room = Room::where('code', $request->code)->first();
+
+    if (!$room) {
+        return "Stanza non trovata!";
+    }
+
+    $roles = ['Contadino', 'Lupo', 'Veggente'];
+    $role = $roles[array_rand($roles)];
+
+    $player = Player::create([
+        'room_id' => $room->id,
+        'name' => $request->name,
+        'role' => $role
+    ]);
+
+    return "Benvenuto {$player->name}! Il tuo ruolo è: {$player->role}";
+});
