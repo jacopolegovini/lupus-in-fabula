474ccc0dcf28bcd5ca4d63aa391b6c06fa3f67e0
jacopolegovini <legovini96@gmail.com>
2025-12-28 17:15:38 +0100

    limited roles assigment applied

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,42 @@ Route::post('/create-room', function () {
 });
 
 use App\Models\Player;
+use Illuminate\Http\Request;
 
-Route::get('/join', function () {
-    return view('join');
-});
-
-Route::post('/join', function (Illuminate\Http\Request $request) {
+Route::post('/join', function (Request $request) {
     $room = Room::where('code', $request->code)->first();
 
     if (!$room) {
         return "Stanza non trovata!";
     }
 
-    $roles = ['Contadino', 'Lupo', 'Veggente'];
-    $role = $roles[array_rand($roles)];
+    // Controlla quanti giocatori ci sono già
+    $existingPlayers = Player::where('room_id', $room->id)->get();
+
+    // Definiamo i ruoli massimi disponibili
+    $maxRoles = ['Lupo' => 1, 'Veggente' => 1, 'Contadino' => 10];
+
+    // Conta quanti giocatori hanno già ogni ruolo
+    $roleCounts = [];
+    foreach ($existingPlayers as $player) {
+        $roleCounts[$player->role] = ($roleCounts[$player->role] ?? 0) + 1;
+    }
+
+    // Calcola ruoli ancora disponibili
+    $availableRoles = [];
+    foreach ($maxRoles as $role => $max) {
+        $count = $roleCounts[$role] ?? 0;
+        if ($count < $max) {
+            $availableRoles[] = $role;
+        }
+    }
+
+    if (empty($availableRoles)) {
+        return "Tutti i ruoli disponibili sono stati assegnati!";
+    }
+
+    // Assegna un ruolo casuale tra quelli disponibili
+    $role = $availableRoles[array_rand($availableRoles)];
 
     $player = Player::create([
         'room_id' => $room->id,
