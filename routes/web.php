a4cc133b0b34effa705bfd0d9c24c7b382fea042
jacopolegovini <legovini96@gmail.com>
2025-12-28 16:55:23 +0100

    created migration and code for room model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+
+use App\Models\Room;
+use Illuminate\Support\Str;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/create-room', function () {
+    $room = Room::create([
+        'code' => strtoupper(Str::random(6))
+    ]);
+
+    return "Stanza creata! Codice: " . $room->code;
 });
